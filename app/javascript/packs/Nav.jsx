2e6a5bac6545844430b84de242d416f2f6a37a51
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-12 23:39:15 -0300

    Create Nav component

---
+++ app/javascript/packs/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Nav = (props) => (
+  <div className="nav">
+    <h1>Where's Waldo</h1>
+  </div>
+);
+
+export default Nav;
--- app/javascript/packs/Routes.jsx
+++ app/javascript/packs/Routes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home";
+import Nav from "./Nav";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
