32851ed00378b56b9edb0322f291fc0b0d4a9aba
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-12 22:55:28 -0300

    Generate Home controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./hello_react";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
