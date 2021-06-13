142b664d9b08efdd70a7cf6c5bf44eb4fc02374d
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-13 15:45:15 -0300

    Add pictures to Home.jsx

--- app/javascript/packs/Home.jsx
+++ app/javascript/packs/Home.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import Wally1 from "../../assets/images/wally1.jpg";
+import Wally2 from "../../assets/images/wally2.jpg";
+import Wally3 from "../../assets/images/wally3.jpg";
+import Wally4 from "../../assets/images/wally4.jpg";
 
 const Home = (props) => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="pictures">
+        <img src={Wally1} alt="First picture" />
+        <img src={Wally2} alt="Second picture" />
+        <img src={Wally3} alt="Third picture" />
+        <img src={Wally4} alt="Fourth picture" />
+      </div>
+    </div>
+  );
 };
 export default Home;
