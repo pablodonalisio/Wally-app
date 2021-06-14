cd6b6b442a5560eb4b18aa6bb795c25462e29c89
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-14 15:23:02 -0300

    Add eslint and prettier to development

--- app/javascript/packs/Home/Home.jsx
+++ app/javascript/packs/Home/Home.jsx
@@ -1,34 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Wally1 from "../../../assets/images/pictures/wally1.jpg";
 import Wally2 from "../../../assets/images/pictures/wally2.jpg";
 import Wally3 from "../../../assets/images/pictures/wally3.jpg";
 import Wally4 from "../../../assets/images/pictures/wally4.jpg";
 
-const Home = (props) => {
-  const [pictureUrls, setPictureUrls] = useState([
-    { img_url: Wally1, link_url: "/pictures/1" },
-    { img_url: Wally2, link_url: "/pictures/2" },
-    { img_url: Wally3, link_url: "/pictures/3" },
-    { img_url: Wally4, link_url: "/pictures/4" },
-  ]);
+const Home = () => {
+  const pictureUrls = [
+    { img_url: Wally1, link_url: "/pictures/1", id: "1" },
+    { img_url: Wally2, link_url: "/pictures/2", id: "2" },
+    { img_url: Wally3, link_url: "/pictures/3", id: "3" },
+    { img_url: Wally4, link_url: "/pictures/4", id: "4" },
+  ];
   return (
     <div className="home">
       <div className="pictures">
-        {pictureUrls.map((picture, idx) => {
-          return (
-            <figure className="picture-container" key={idx}>
-              <img
-                className="picture"
-                src={picture.img_url}
-                alt="miniature wally pic"
-              />
-              <div className="link-container">
-                <Link to={picture.link_url}>PLAY</Link>
-              </div>
-            </figure>
-          );
-        })}
+        {pictureUrls.map((picture) => (
+          <figure className="picture-container" key={picture.id}>
+            <img
+              className="picture"
+              src={picture.img_url}
+              alt="miniature wally pic"
+            />
+            <div className="link-container">
+              <Link to={picture.link_url}>PLAY</Link>
+            </div>
+          </figure>
+        ))}
       </div>
     </div>
   );
