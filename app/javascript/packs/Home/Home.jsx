b341c2631e4587020efc580aab2daa9b30c6d945
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-21 17:20:12 -0300

    Fix link_urls in Home.jsx

--- app/javascript/packs/Home/Home.jsx
+++ app/javascript/packs/Home/Home.jsx
@@ -4,16 +4,18 @@ import Wally1 from "../../../assets/images/pictures/wally1.jpg";
 import Wally2 from "../../../assets/images/pictures/wally2.jpg";
 import Wally3 from "../../../assets/images/pictures/wally3.jpg";
 import Wally4 from "../../../assets/images/pictures/wally4.jpg";
+import Nav from "./Nav";
 
 const Home = () => {
   const pictureUrls = [
-    { img_url: Wally1, link_url: "/pictures/1", id: "1" },
-    { img_url: Wally2, link_url: "/pictures/2", id: "2" },
-    { img_url: Wally3, link_url: "/pictures/3", id: "3" },
-    { img_url: Wally4, link_url: "/pictures/4", id: "4" },
+    { img_url: Wally1, link_url: "/pictures/0", id: "0" },
+    { img_url: Wally2, link_url: "/pictures/1", id: "1" },
+    { img_url: Wally3, link_url: "/pictures/2", id: "2" },
+    { img_url: Wally4, link_url: "/pictures/3", id: "3" },
   ];
   return (
     <div className="home">
+      <Nav />
       <div className="pictures">
         {pictureUrls.map((picture) => (
           <figure className="picture-container" key={picture.id}>
