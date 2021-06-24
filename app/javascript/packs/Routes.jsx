021d2a8005e4995b9124a7aa77615c0c372dbd9b
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-24 20:51:55 -0300

    Refactor Routes.jsx

--- app/javascript/packs/Routes.jsx
+++ app/javascript/packs/Routes.jsx
@@ -1,17 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import Picture from "./Picture/Picture";
 
-const Routes = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/pictures/:pictureId" component={Picture} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/pictures/:pictureId" component={Picture} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Routes;
