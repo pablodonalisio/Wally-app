84a0b226f67d2b9972ac71d754fc367093e269f7
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-24 21:05:49 -0300

    Add validations spec for CoordinatePair model

---
+++ spec/models/coordinate_pair_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe CoordinatePair, type: :model do
+  describe 'validations' do
+    let(:character) { Character.create({ name: 'Wally' }) }
+    let(:picture) { Picture.create({ url: 'some/url.jpg' }) }
+    it 'is valid with valid attributes' do
+      coordinate_pair = CoordinatePair.new(
+        {
+          x: '50',
+          y: '50',
+          character_id: character.id,
+          picture_id: picture.id
+        }
+      )
+      expect(coordinate_pair).to be_valid
+    end
+
+    it { should validate_presence_of(:x) }
+    it { should validate_presence_of(:x) }
+    it { should validate_presence_of(:picture_id) }
+    it { should validate_presence_of(:character_id) }
+  end
+end
