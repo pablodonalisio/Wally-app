a9098650aa5c42aec5214d7a560ea3efd3706ac6
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-25 20:53:25 -0300

    Add associations between Pictures, CoordinatePairs and Characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ApplicationRecord
   validates_presence_of :name
+  has_many :coordinate_pairs
+  has_many :pictures, through: :coordinate_pairs
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
   validates_presence_of :name
   has_one_attached :image
+  has_many :coordinate_pairs
+  has_many :characters, through: :coordinate_pairs
 end
