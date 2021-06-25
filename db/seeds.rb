b3d5df057cd3b98d74e66ee39274d9fc5fe727df
Pablo Donalisio <pablo.donalisio@gmail.com>
2021-06-25 17:07:09 -0300

    Add seeds for Pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Picture.destroy_all
+
+(1..4).each do |n|
+  picture = Picture.new({ name: "Wally#{n}" })
+  picture.image.attach(
+    io: File.open(Rails.root.join('app', 'assets', 'images', 'pictures', "wally#{n}.jpg")),
+    filename: "Wally#{n}.jpg",
+    content_type: 'image/jpg'
+  )
+  picture.save
+end
